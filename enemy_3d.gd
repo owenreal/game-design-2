8019b422580b35853871ad66246557d701967e2a
owenreal <162478543+owenreal@users.noreply.github.com>
2025-02-21 09:39:31 -0600

    2/21

--- enemy_3d.gd
+++ enemy_3d.gd
@@ -25,24 +25,25 @@ func _physics_process(delta: float) -> void:
 			SPEED = RUNSPEED
 		else:
 			SPEED = WALKSPEED
-		if nav_agent.distance_to_target() == 0:
+		if nav_agent.distance_to_target() == 1:
 			nav_agent.target_position = global_position
+			
 		if atk_area.overlaps_body(player):
 			player.take_damage(ATTACK)
 			player.inertia = (player.global_position-global_position)\
 								.normalized() * KNOCKBACK
 	var dir = (nav_agent.target_position-self.global_position)
 	dir.y = 0
-	if dir.length() > 0.01 and nav_agent.target_position != global_position:
+	if dir.length() > 0.01:
 		var rot_angle = atan2(dir.x, dir.z)
 		self.rotation.y = lerp_angle(rotation.y, rot_angle, 5 * delta)
-		velocity = velocity.lerp(dir.normalized() * SPEED, ACCEL * delta)
+	velocity = velocity.lerp(dir.normalized() * SPEED, ACCEL * delta)
 	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	if velocity.length() <= 1:
-		nav_agent.target_position = global_position
+		nav_agent.target_position = self.global_position
 		animator.play("Idle")
 	elif SPEED == WALKSPEED:
 		animator.play("UnarmedWalkForward")
