a79682267102702ef4cc777f56ba1d2f15917a5d
owenreal <162478543+owenreal@users.noreply.github.com>
2025-04-14 09:38:28 -0500

    4/14

--- rc_player.gd
+++ rc_player.gd
@@ -1,9 +1,9 @@
 extends VehicleBody3D
 
 const MAX_STEER = 0.4
-const MAX_RPM = 500
+const MAX_RPM = 400
 const MAX_TORQUE = 200
-const HORSE_POWER = 300
+const HORSE_POWER = 200
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
@@ -13,19 +13,26 @@ func calc_engine_force(accel, rpm):
 
 func _physics_process(delta: float) -> void:
 	steering = lerp(steering,
-						Input.get_axis("ui_right", "ui_left") * MAX_STEER,
-						delta * 5.0)
+					Input.get_axis("ui_right", "ui_left") * MAX_STEER,
+					delta * 5.0)
 	var accel = Input.get_axis("ui_down", "ui_up") * HORSE_POWER
-	$back_left.engine_force = calc_engine_force(accel, abs($back_left.get_rpm()))
-	$back_right.engine_force = calc_engine_force(accel, abs($back_right.get_rpm()))
+	$backLeft.engine_force = calc_engine_force(accel, abs($backLeft.get_rpm()))
+	$backRight.engine_force = calc_engine_force(accel, abs($backRight.get_rpm()))
 	
 	var fwd_mps = abs((linear_velocity * transform.basis).z)
 	$Label.text = "%d mph" % (fwd_mps * 2.23694)
 	
-	$center_mass.global_position = $center_mass.global_position.lerp(
-												global_position, delta * 20.0)
-	$center_mass.transform = $center_mass.transform.interpolate_with(
-											transform, delta * 5.0)
-	$center_mass/Camera3D.look_at(global_position.lerp(
-								global_position + linear_velocity, delta * 5.0))
-	#TODO: check and right
+	$centerMass.global_position = $centerMass.global_position.lerp(
+											   global_position, delta * 20.0)
+	$centerMass.transform = $centerMass.transform.interpolate_with(
+										 transform, delta * 5.0)
+	$centerMass/Camera3D.look_at(global_position.lerp(
+								  global_position + linear_velocity, delta * 5.0))
+	check_and_right()
+
+func check_and_right():
+	if global_transform.basis.y.dot(Vector3.UP) < 0:
+		var cur_rotation = self.rotation_degrees
+		cur_rotation.x = 0  # Reset pitch
+		cur_rotation.z = 0  # Reset roll
+		self.rotation_degrees = cur_rotation
